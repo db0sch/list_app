90fcfcfb880936306090df7acaadf676d1a7ca26
Dimitri Bosch <dimitribosch@hotmail.com>
2016-09-06 16:28:52 +0200

    trying to sorting the mess in this repo

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,47 @@
+class CollectionsController < ApplicationController
+  before_action :set_collection, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @collections = Collection.all
+  end
+
+  def show
+
+  end
+
+  def new
+    raise
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    @collection.user = current_user
+    @collection.save
+    redirect_to collection_path(@collection)
+  end
+
+  def edit
+  end
+
+  def update
+    @collection.update(collection_params)
+    redirect_to collection_path(@collection)
+  end
+
+  def destroy
+    @collection.destroy
+    redirect_to collections_path
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:title, :description)
+  end
+
+  def set_collection
+    @collection = Collection.find(params[:id])
+  end
+
+end
---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,52 @@
+class ResourcesController < ApplicationController
+  before_action :set_resource, only: [:show, :edit, :update, :destroy]
+  before_action :set_collection, only: [:new, :create, :edit, :update, :destroy]
+
+
+  def index
+    # no use of index
+  end
+
+  def show
+    # the @resource variable has been set with the before_action 'set_resource' method.
+  end
+
+  def new
+    @resource = Resource.new
+  end
+
+  def create
+    @resource = Resource.new(resource_params)
+    @resource.collection = @collection
+    @resource.save
+    redirect_to collection_path(@collection)
+  end
+
+  def edit
+    # the @resource and @collection variable have been set with the before_action 'set_link' and 'set_collection' methods.
+  end
+
+  def update
+    @resource.update(resource_params)
+    redirect_to collection_path(@collection)
+  end
+
+  def destroy
+    @resource.destroy
+    redirect_to collection_path(@collection)
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:title, :content, :uri)
+  end
+
+  def set_resource
+    @resource = Resource.find(params[:id])
+  end
+
+  def set_collection
+    @collection = Collection.find(params[:collection_id])
+  end
+end
