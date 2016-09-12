2cc1cd71e971f809a7e34dab31bf8c13bd5cd34d
Dimitri Bosch <dimitribosch@hotmail.com>
2016-09-12 16:42:11 +0200

    set up the follow method in user controller and the routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :follow]
 
   def show
+    # @user set by the before_action method
+  end
 
+  # Non-CRUD actions
+
+  def follow
+    current_user.follow(@user)
+    redirect_to :back
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,9 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :update, :destroy]
   end
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    member do
+      put "follow", to: "users#follow"
+    end
+  end
 end
