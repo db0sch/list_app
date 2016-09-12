5198b8942cc96d63c41800f227240cba3c0d9c8a
Dimitri Bosch <dimitribosch@hotmail.com>
2016-09-12 14:37:47 +0200

    added comments sys to resource.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   before_action :set_resource, only: [:create, :update, :destroy]
+  before_action :set_comment, only: [:update, :destroy]
 
   def create
     @comment = Comment.new(comment_params)
     @comment.commentable = @resource
+    @comment.user = current_user
     if @comment.save
       redirect_to collection_resource_path(@resource.collection, @resource)
     else
@@ -16,6 +18,8 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment.destroy
+    redirect_to collection_resource_path(@resource.collection, @resource)
 
   end
 
@@ -23,6 +27,11 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+    authorize @comment
   end
 
   def set_resource
