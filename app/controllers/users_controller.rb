97789c20d0283d5ad0542a83e93474114c96f0f3
Dimitri Bosch <dimitribosch@hotmail.com>
2016-09-22 15:09:13 +0200

    first draft of the profile page with the follow btn for other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :follow]
+  before_action :set_user, only: [:show, :follow, :follow]
 
   def show
     # @user set by the before_action method
@@ -8,8 +8,16 @@ class UsersController < ApplicationController
   # Non-CRUD actions
 
   def follow
-    current_user.follow(@user)
-    redirect_to user_path(@user)
+    if current_user.following? @user
+      current_user.stop_following @user
+    else
+      current_user.follow @user
+    end
+
+    respond_to do |format|
+      format.js  # <-- users/follow.js.erb
+    end
+
   end
 
   private
