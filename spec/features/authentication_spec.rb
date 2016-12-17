4cb5306310dbba4ab1e2bbc76a096ffbcf628d8f
Dimitri Bosch <dimitribosch@hotmail.com>
2016-12-17 19:50:23 +0900

    add some specs for testing devise. still need some work. fix #65

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -27,4 +27,40 @@ RSpec.feature "Authentication", type: :feature do
     expect(page).to have_text("Please review the problems below:")
     expect(page).to have_text("can't be blank")
   end
+
+  scenario "User can sign in" do
+    user = create(:user)
+
+    visit "/users/sign_in"
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Log in"
+    expect(page).to have_text("Signed in successfully.")
+  end
+
+  scenario "User can't sign in with invalid informations" do
+    visit "/users/sign_in"
+
+    fill_in "Email", with: Faker::Internet.email
+    fill_in "Password", with: Faker::Internet.password
+
+    click_button "Log in"
+    expect(page).to have_text("Invalid Email or password")
+  end
+
+  scenario "User can't sign in with missing informations" do
+    visit "/users/sign_in"
+
+    fill_in "Email", with: Faker::Internet.email
+
+    click_button "Log in"
+    expect(page).to have_text("Invalid Email or password")
+  end
+
+  scenario "User can edit his profile" do
+    # visit "/users/edit"
+    # test with a user, change the name, and check if the name is different after.
+  end
 end
