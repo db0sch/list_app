f2ea4d63c08a2bccd90172902dcbb8d69d17427e
Dimitri Bosch <dimitribosch@hotmail.com>
2016-12-19 16:22:18 +0900

    specs for user update and password update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
   end
 
   def skip_pundit?
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -60,7 +60,56 @@ RSpec.feature "Authentication", type: :feature do
   end
 
   scenario "User can edit his profile" do
+    user = create(:user)
+    login_as(user, :scope => :user)
+
+    visit "/users/edit"
+
+    new_first_name = "Peter"
+    new_last_name = "Lorre"
+    new_username = "peterl"
+    new_email = "peterl@example.com"
+
+    fill_in "First name", with: new_first_name
+    fill_in "Last name", with: new_last_name
+    fill_in "Username", with: new_username
+    fill_in "Email", with: new_email
+    fill_in "Current password", with: user.password
+
+    click_button "Update"
+    expect(page).to have_text("Your account has been updated successfully.")
+
+    user = user.reload
+
+    expect(user.first_name == new_first_name).to be true
+    expect(user.last_name == new_last_name).to be true
+    expect(user.username == new_username).to be true
+    expect(user.email == new_email).to be true
+  end
+
+  scenario "User can change their password" do
+    user = create(:user)
+    login_as(user, :scope => :user)
+
+    visit "/users/edit"
+
+    new_password = "D{[Hhn6XL8h,xc7}vGy"
+
+    fill_in "Password", with: new_password, match: :prefer_exact
+    fill_in "Password confirmation", with: new_password, match: :prefer_exact
+    fill_in "Current password", with: user.password, match: :prefer_exact
+
+    click_button "Update"
+    expect(page).to have_text("Your account has been updated successfully.")
+
+    user = user.reload
+
+    expect(user.valid_password?(new_password)).to be true
+  end
+
+  scenario "User can sign out" do
     # visit "/users/edit"
     # test with a user, change the name, and check if the name is different after.
   end
+
 end
