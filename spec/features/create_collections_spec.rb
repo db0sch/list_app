7aea0d2144b8d3ff975451882930dfb7d6f50eec
Dimitri Bosch <dimitribosch@hotmail.com>
2016-12-20 23:32:47 +0900

    new specs for Collection feature

--- spec/features/create_collections_spec.rb
+++ spec/features/create_collections_spec.rb
@@ -11,41 +11,49 @@ RSpec.feature "CreateCollections", type: :feature do
 
     visit "/collections/new"
 
-    collection_title = "Best news website"
+    title = "Best news website"
     description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    tagline = "this is the tagline"
+    status = "Open"
 
-    fill_in "Title", with: collection_title
+    fill_in "Title", with: title
     fill_in "Description", with: description
+    fill_in "Tagline", with: tagline
+    choose status
     click_button "create a collection"
-    # fill_in "Resource title", with: resource_title
-    # fill_in "Resource content", with: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident inventore, ipsum nobis quibusdam asperiores eveniet possimus, ducimus voluptatum, fugiat voluptate reprehenderit. Illo, commodi? Ex tenetur, voluptatum eos a soluta voluptates."
-    # fill_in "Resource uri", with: "https://www.bloomberg.com/"
-    expect(page).to have_text(collection_title)
+
+    expect(page).to have_text(title)
     expect(page).to have_text(description)
+    expect(page).to have_text(tagline)
     expect(page).to have_link("add a resource")
     expect(page).to have_text("Comments")
+    expect(page).to have_text(status)
   end
 
   scenario "user can edit a collection" do
     user = create(:user)
     login_as(user, :scope => :user)
-    p user.id
+
     collection = create(:collection, user: user)
-    p collection
-    visit "/collections/#{collection.id}"
 
-    collection_title = "New title for testing, hey"
+    visit "/collections/#{collection.id}/edit"
+
+    title = "New title for testing, hey"
     description = "Hello, I love you, can you tell me your name?"
+    tagline = "another tagline"
+    status = "Private"
 
-    fill_in "Title", with: collection_title
+    fill_in "Title", with: title
     fill_in "Description", with: description
+    fill_in "Tagline", with: tagline
+    choose status
     click_button "create a collection"
-    # fill_in "Resource title", with: resource_title
-    # fill_in "Resource content", with: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident inventore, ipsum nobis quibusdam asperiores eveniet possimus, ducimus voluptatum, fugiat voluptate reprehenderit. Illo, commodi? Ex tenetur, voluptatum eos a soluta voluptates."
-    # fill_in "Resource uri", with: "https://www.bloomberg.com/"
-    expect(page).to have_text(collection_title)
+
+    expect(page).to have_text(title)
     expect(page).to have_text(description)
+    expect(page).to have_text(tagline)
     expect(page).to have_link("add a resource")
     expect(page).to have_text("Comments")
+    expect(page).to have_text(status)
   end
 end
