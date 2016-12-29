d0268a55f2f93e37465b52f6703d86586df121e3
Dimitri Bosch <dimitribosch@hotmail.com>
2016-12-29 15:19:50 +0900

    first specs for resources features

--- spec/features/resources_spec.rb
+++ spec/features/resources_spec.rb
@@ -3,11 +3,36 @@ require 'rails_helper'
 RSpec.feature "Resources", type: :feature do
 
   scenario "A visitor can see a resource (if collection is public or open)" do
-    # TODO
+    # visitor not logged in
+    user = create(:user)
+    collection = create(:collection, user: user)
+    resource = create(:resource, collection: collection)
+    # can access and view a collection
+    visit "/collections/#{collection.id}/resources/#{resource.id}"
+    expect(page).to have_text(resource.title)
+    expect(page).to have_text(resource.content)
+    expect(page).to have_text(resource.uri)
+    # same specs for collection with status: :is_open
+    # visitor not logged in
+    collection2 = create(:collection, user: user, status: :is_open)
+    resource2 = create(:resource, collection: collection2)
+    # can access and view a collection
+    visit "/collections/#{collection2.id}/resources/#{resource2.id}"
+    expect(page).to have_text(resource.title)
+    expect(page).to have_text(resource.content)
+    expect(page).to have_text(resource.uri)
   end
 
-  scenario "A visitor cannot see a resource if collection is private" do
-    # TODO
+  scenario "A visitor can see a resource if collection is private" do
+    # visitor not logged in
+    user = create(:user)
+    collection = create(:collection, user: user, status: :is_private)
+    resource = create(:resource, collection: collection)
+    # can access and view a collection
+    visit "/collections/#{collection.id}/resources/#{resource.id}"
+    expect(page).to have_text(resource.title)
+    expect(page).to have_text(resource.content)
+    expect(page).to have_text(resource.uri)
   end
 
   scenario "A visitor cannot interact with the resource (comment, edit or delete)" do
